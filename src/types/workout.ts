6f6ca48dbaeb99f037511fd5ae7663775933e34c
sho-ki <shoki0116.highjump@gmail.com>
2021-10-15 00:35:56 +0900

    add workout type

--- src/types/workout.ts
+++ src/types/workout.ts
@@ -1,3 +1,19 @@
-export type Workout = {
-    
-}
+export type PostUserWorkout = {
+  id: number;
+  name: string;
+  category: string;
+  sets: number;
+  times: number;
+  orderIndex: number;
+};
+
+export type WorkoutItem = {
+  userId: number;
+  user_workouts_id: number;
+  training_name: string;
+  sets: number;
+  times: number;
+  order_index: number;
+  label: string;
+  scheduled_day: 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | null;
+};
